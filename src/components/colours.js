3cfc6383d73a3fc63bbec95c4a9901b55c47e5b6
brandonPlaza <plazab@sheridancollege.ca>
2023-09-29 19:07:17 -0400

    Added styling to search bar for current location

---
+++ src/components/colours.js
@@ -0,0 +1,5 @@
+export const COLOURS = {
+    grey:'#D9D9D9',
+    black:'#000000',
+    redpink:"#FF455B"
+}
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Platform, Text, View, SafeAreaView, TextInput } from 'react-native';
+
+// Imports the stylesheet that applies global styles for things that should remain consistent like the SafeAreaView fix
 import GlobalStyleSheet from '../../components/GlobalStyleSheet';
+import { COLOURS } from '../../components/colours';
 
 export default function HomeScreen() {
-  return (
-    <SafeAreaView style={GlobalStyleSheet.androidSafeAreaView}>
-        <Text>Hello World!</Text>
-    </SafeAreaView>
-  );
+    const [currLocationSearch, onChangeText] = React.useState('');
+
+    return (
+        <SafeAreaView style={GlobalStyleSheet.androidSafeAreaView}>
+            <TextInput
+                style={styles.textInput}
+                cursorColor={COLOURS.redpink}
+                onChangeText={onChangeText}
+                value={currLocationSearch}
+            />
+        </SafeAreaView>
+    );
 }
+
+const styles = StyleSheet.create({
+    textInput: {
+        backgroundColor: "#E5E5E5",
+        marginStart: 10,
+        marginEnd: 10,
+
+        padding: 5,
+
+        borderRadius: 10
+    }
+});
