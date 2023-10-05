93dd53f096807a7cee76554080627a34bafbddb5
Brandon Plaza <plazab@sheridancollege.ca>
2023-10-05 13:45:28 -0400

    Completed styling of button for access to routing

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,43 +1,58 @@
 import React from 'react';
 import { StyleSheet, Platform, Text, View, SafeAreaView, TextInput, TouchableOpacity } from 'react-native';
 
-import {} from 'feather-icons'
+import { ArrowRight } from 'react-native-feather'
 
 // Imports the stylesheet that applies global styles for things that should remain consistent like the SafeAreaView fix
 import GlobalStyleSheet from '../../components/GlobalStyleSheet';
 import { COLOURS } from '../../components/colours';
 
 export default function HomeScreen() {
-    const [currLocationSearch, onChangeText] = React.useState('');
-
+  const [currLocationSearch, onChangeText] = React.useState('');
     return (
-        <SafeAreaView style={GlobalStyleSheet.androidSafeAreaView}>
-            {/* <TextInput
-                style={styles.textInput}
-                cursorColor={COLOURS.redpink}
-                onChangeText={onChangeText}
-                value={currLocationSearch}
-            /> */}
-            <TouchableOpacity
-                style={styles.getRouteButton}
-            >
-                <Text style={styles.getRouteButtonText}>Find your destination</Text>
-            </TouchableOpacity>
-        </SafeAreaView>
+      <SafeAreaView style={[GlobalStyleSheet.androidSafeAreaView]}>
+        {/* <TextInput
+        style={styles.textInput}
+        cursorColor={COLOURS.redpink}
+        onChangeText={onChangeText}
+        value={currLocationSearch}
+        /> */}
+        <View style={styles.getRouteButtonHeader}>
+          <TouchableOpacity
+            style={styles.getRouteButton}
+          >
+            <Text style={styles.getRouteButtonText}>Find your destination</Text>
+            <ArrowRight stroke={COLOURS.black} width={38} height={38} />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
+    getRouteButtonHeader:{
+      height: "auto",
+      overflow: "hidden",
+      padding:5
+    },
     getRouteButton: {
-        backgroundColor: "#E5E5E5",
-        marginStart: 10,
-        marginEnd: 10,
+      display:"flex",
+      flexDirection:"row",
 
-        padding: 5,
+      justifyContent:"space-between",
 
-        borderRadius: 10,
+      backgroundColor: "#E5E5E5",
+      marginStart: 10,
+      marginEnd: 10,
+
+      paddingStart: 10,
+      paddingTop: 7,
+      paddingEnd: 10,
+      paddingBottom: 7,
+
+      borderRadius: 8,
     },
     getRouteButtonText:{
-        fontSize:30 
+      fontSize:30 
     }
 });
