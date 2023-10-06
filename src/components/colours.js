261230e1d5a4259207173d3818294a7a35b95312
Brandon Plaza <plazab@sheridancollege.ca>
2023-10-06 16:38:23 -0400

    Created a better search bar using SearchBar from react-native-elements, and used their example code to create a basic example of searching from a return value from an API call

--- src/components/colours.js
+++ src/components/colours.js
@@ -1,5 +1,7 @@
 export const COLOURS = {
     grey:'#D9D9D9',
     black:'#000000',
+    white:'#FFFFFF',
+    light_grey:'#EFEFEF',
     redpink:"#FF455B"
 }
--- src/screens/CurrentLocationScreen/CurrentLocationScreen.js
+++ src/screens/CurrentLocationScreen/CurrentLocationScreen.js
@@ -1,36 +1,122 @@
-import React from 'react';
-import { StyleSheet, Platform, Text, View, SafeAreaView, TextInput } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Platform, Text, View, SafeAreaView, TextInput, FlatList } from 'react-native';
+import { SearchBar } from 'react-native-elements'
 
 // Imports the stylesheet that applies global styles for things that should remain consistent like the SafeAreaView fix
 import GlobalStyleSheet from '../../components/GlobalStyleSheet';
 import { COLOURS } from '../../components/colours';
+import { color } from 'react-native-elements/dist/helpers';
 
 export default function CurrentLocationScreen({navigation}){
-  const [currLocationSearch, onChangeText] = React.useState('');
+  const [search, setSearch] = useState('');
+  const [filteredDataSource, setFilteredDataSource] = useState([]);
+  const [masterDataSource, setMasterDataSource] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setFilteredDataSource(responseJson);
+        setMasterDataSource(responseJson);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+  
+  const searchFilterFunction = (text) => {
+    // Check if searched text is not blank
+    if (text) {
+      // Inserted text is not blank
+      // Filter the masterDataSource
+      // Update FilteredDataSource
+      const newData = masterDataSource.filter(function (item) {
+        const itemData = item.title
+          ? item.title.toUpperCase()
+          : ''.toUpperCase();
+        const textData = text.toUpperCase();
+        return itemData.indexOf(textData) > -1;
+      });
+      setFilteredDataSource(newData);
+      setSearch(text);
+    } else {
+      // Inserted text is blank
+      // Update FilteredDataSource with masterDataSource
+      setFilteredDataSource(masterDataSource);
+      setSearch(text);
+    }
+  };
+
+  const ItemView = ({ item }) => {
+    return (
+      // Flat List Item
+      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
+        {item.id}
+        {'.'}
+        {item.title.toUpperCase()}
+      </Text>
+    );
+  };
+
+  const ItemSeparatorView = () => {
+    return (
+      // Flat List Item Separator
+      <View
+        style={{
+          height: 0.5,
+          width: '100%',
+          backgroundColor: '#C8C8C8',
+        }}
+      />
+    );
+  };
+
+  const getItem = (item) => {
+    // Function for click on an item
+    alert('Id : ' + item.id + ' Title : ' + item.title);
+  };
+
   return(
     <SafeAreaView style={[GlobalStyleSheet.androidSafeAreaView]}>
-      <TextInput
+      {/* <TextInput
         style={styles.textInput}
         cursorColor={COLOURS.redpink}
         onChangeText={onChangeText}
         value={currLocationSearch}
+      /> */}
+      <SearchBar
+        round
+        searchIcon={{ size: 24 }}
+        onChangeText={(text) => searchFilterFunction(text)}
+        onClear={(text) => searchFilterFunction('')}
+        placeholder="Where are you?..."
+        value={search}
+        containerStyle={styles.searchBoxContainer}
+        inputContainerStyle={styles.searchBoxInputContainer}
+        inputStyle={styles.searchBoxInput}
+      />
+      <FlatList
+        data={filteredDataSource}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={ItemSeparatorView}
+        renderItem={ItemView}
       />
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  textInput:{
+  searchBoxContainer:{
+    backgroundColor: COLOURS.light_grey,
+    borderColor:COLOURS.light_grey
+  },
+  searchBoxInputContainer:{
     backgroundColor: COLOURS.grey,
-
-    paddingStart: 5,
-    paddingTop: 5,
-    paddingEnd: 5,
-    paddingBottom:5,
-    
-    marginStart:10,
-    marginEnd:10,
-
-    fontSize:25
+  },
+  searchBoxInput:{
+    color:COLOURS.black,
+  },
+  itemStyle: {
+    padding: 10,
   },
 })
