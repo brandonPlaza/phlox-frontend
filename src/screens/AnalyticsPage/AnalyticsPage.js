9ac8155bdfdfe8068ddbc90fd71d17514fe3cdb6
DylanDabrowski <dylandabrowski@gmail.com>
2023-10-15 13:59:44 -0400

    implemented charts for the data analytics

--- src/screens/AnalyticsPage/AnalyticsPage.js
+++ src/screens/AnalyticsPage/AnalyticsPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, ScrollView, SafeAreaView } from "react-native";
 import axios from "axios";
+import { PieChart } from "react-native-chart-kit";
 
 import data from "./data.json";
 
@@ -63,19 +64,58 @@ const AnalyticsPage = () => {
     let result = [];
     for (let type in outOfServiceCounts) {
       const count = outOfServiceCounts[type];
-      result.push(`${count} ${type} node(s) marked out of service`);
+      result.push(`${count} ${type}'s`);
     }
 
-    return result.join(", \n");
+    return result.join("\n");
   };
 
+  // Without Pie Chart
+  // const overallObstructionHistorySpread = () => {
+  //   const totalOutOfServiceNodes = nodes.filter(
+  //     (node) => node.isOutOfService
+  //   ).length;
+
+  //   if (totalOutOfServiceNodes === 0) {
+  //     return "No Nodes are Out of Service!";
+  //   }
+
+  //   let outOfServiceCounts = {};
+  //   nodeTypes.forEach((type) => {
+  //     outOfServiceCounts[type] = 0;
+  //   });
+
+  //   nodes.forEach((node) => {
+  //     if (node.isOutOfService) {
+  //       const nodeTypeName = getNodeTypeName(node.nodeType);
+  //       outOfServiceCounts[nodeTypeName]++;
+  //     }
+  //   });
+
+  //   let result = [];
+  //   for (let type in outOfServiceCounts) {
+  //     const count = outOfServiceCounts[type];
+  //     const percentage = ((count / totalOutOfServiceNodes) * 100).toFixed(2);
+  //     result.push(
+  //       `${count}/${totalOutOfServiceNodes} (${percentage}%) are ${type}`
+  //     );
+  //   }
+
+  //   return result.join(", \n");
+  // };
+
+  // With Pie Chart
   const overallObstructionHistorySpread = () => {
     const totalOutOfServiceNodes = nodes.filter(
       (node) => node.isOutOfService
     ).length;
 
     if (totalOutOfServiceNodes === 0) {
-      return "No Nodes are Out of Service!";
+      return (
+        <View>
+          <Text>No Nodes are Out of Service!</Text>
+        </View>
+      );
     }
 
     let outOfServiceCounts = {};
@@ -90,16 +130,55 @@ const AnalyticsPage = () => {
       }
     });
 
-    let result = [];
+    const colors = [
+      "#E63946", // Crimson
+      "#F4A261", // Sandy Brown
+      "#2A9D8F", // Light Sea Green
+      "#264653", // Charcoal
+      "#8AC926", // Lime Green
+      "#FF61A6", // Pink
+      "#1B4332", // British Racing Green
+      "#F72585", // Neon Fuchsia
+    ];
+
+    let chartData = [];
+    let colorIndex = 0;
     for (let type in outOfServiceCounts) {
       const count = outOfServiceCounts[type];
-      const percentage = ((count / totalOutOfServiceNodes) * 100).toFixed(2);
-      result.push(
-        `${count}/${totalOutOfServiceNodes} (${percentage}%) are ${type}`
-      );
+      chartData.push({
+        name: type,
+        population: count,
+        color: colors[colorIndex % colors.length],
+        legendFontColor: "#7F7F7F",
+        legendFontSize: 15,
+      });
+      colorIndex++;
     }
 
-    return result.join(", \n");
+    return (
+      <View>
+        <PieChart
+          data={chartData}
+          width={400} // Dimensions.get("window").width
+          height={220}
+          chartConfig={{
+            backgroundColor: "#ffffff",
+            backgroundGradientFrom: "#ffffff",
+            backgroundGradientTo: "#ffffff",
+            decimalPlaces: 2, // optional, defaults to 2dp
+            color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+            style: {
+              borderRadius: 16,
+            },
+          }}
+          accessor={"population"}
+          backgroundColor={"transparent"}
+          padding={"20"}
+          center={[10, 10]}
+          absolute
+        />
+      </View>
+    );
   };
 
   const averageResolutionTime = () => {
@@ -138,10 +217,26 @@ const AnalyticsPage = () => {
         ? `${(totalResolutionTime / resolutionCount).toFixed(2)} hours`
         : "No resolved entries found";
 
-    return {
-      totalAverageTime,
-      individualAverageTimes,
-    };
+    return (
+      <View style={styles.tableContainer}>
+        <View style={styles.tableRow}>
+          <Text style={styles.tableHeader}>Node Name</Text>
+          <Text style={styles.tableHeader}>Average Resolution Time</Text>
+        </View>
+        {Object.entries(individualAverageTimes).map(
+          ([nodeName, averageTime]) => (
+            <View key={nodeName} style={styles.tableRow}>
+              <Text style={styles.tableCell}>{nodeName}</Text>
+              <Text style={styles.tableCell}>{averageTime}</Text>
+            </View>
+          )
+        )}
+        <View style={styles.tableRow}>
+          <Text style={styles.tableFooter}>Total Average Time</Text>
+          <Text style={styles.tableFooter}>{totalAverageTime}</Text>
+        </View>
+      </View>
+    );
   };
 
   const frequentlyUsedNodes = () => {
@@ -157,33 +252,33 @@ const AnalyticsPage = () => {
           <Text style={styles.metricTitle}>
             Frequency of Node Out of Service in the Past 30 Days
           </Text>
+          <Text style={styles.metricSubtitle}>
+            How many of each time of obstruction has gone out of service is the
+            last month.
+          </Text>
           <Text style={styles.metricText}>{frequencyOfNodeOutOfService()}</Text>
         </View>
         <View style={styles.metricContainer}>
           <Text style={styles.metricTitle}>
             Overall Obstruction History Spread
           </Text>
-          <Text style={styles.metricText}>
-            {overallObstructionHistorySpread()}
+          <Text style={styles.metricSubtitle}>
+            All currently Out Of Service nodes currently on campus.
           </Text>
+          {overallObstructionHistorySpread()}
         </View>
         <View style={styles.metricContainer}>
           <Text style={styles.metricTitle}>Average Resolution Time</Text>
-          <Text style={styles.metricText}>
-            Total: {resolutionTimes.totalAverageTime}
+          <Text style={styles.metricSubtitle}>
+            Average time it takes on average for each node out of service to be
+            resolved.
           </Text>
-          {Object.entries(resolutionTimes.individualAverageTimes).map(
-            ([nodeName, averageTime]) => (
-              <Text key={nodeName} style={styles.metricText}>
-                {nodeName}: {averageTime}
-              </Text>
-            )
-          )}
+          {averageResolutionTime()}
         </View>
-        <View style={styles.metricContainer}>
+        {/* <View style={styles.metricContainer}>
           <Text style={styles.metricTitle}>Frequently Used Nodes</Text>
           <Text style={styles.metricText}>{frequentlyUsedNodes()}</Text>
-        </View>
+        </View> */}
       </ScrollView>
     </SafeAreaView>
   );
@@ -203,6 +298,12 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: "bold",
   },
+  metricSubtitle: {
+    fontSize: 16,
+    color: "#666",
+    fontStyle: "italic",
+    paddingBottom: 15,
+  },
   metricText: {
     fontSize: 16,
   },
@@ -212,6 +313,36 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#ddd",
     borderRadius: 5,
+  },
+
+  // table
+  tableContainer: {
+    borderWidth: 1,
+    borderColor: "#ddd",
+  },
+  tableRow: {
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: "#ddd",
+  },
+  tableHeader: {
+    flex: 1,
+    padding: 8,
+    fontWeight: "bold",
+    backgroundColor: "#f0f0f0",
+    textAlign: "center",
+  },
+  tableCell: {
+    flex: 1,
+    padding: 8,
+    textAlign: "center",
+  },
+  tableFooter: {
+    flex: 1,
+    padding: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#e0e0e0",
   },
 });
 
